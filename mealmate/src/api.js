1ecb88e5d315c08fad7a928ef0815dea585e4d42
FTFati <fatima.amkachou@gmail.com>
2025-01-26 16:14:53 +0100

    create api file and add api key

---
+++ mealmate/src/api.js
@@ -0,0 +1,12 @@
+const API_KEY = 'f884b7ff00ec4c448873fed5048e444d';
+const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
+
+export async function fetchRecipes(ingredient) {
+    try {
+        const response = await fetch(`${BASE_URL}?query=${ingredient}&number=10&apiKey=${API_KEY}`);
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+    }
+}
