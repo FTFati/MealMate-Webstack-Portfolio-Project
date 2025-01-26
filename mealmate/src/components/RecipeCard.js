e070dc9f50e8d3a8604d53da0531653dc1fa6fee
FTFati <fatima.amkachou@gmail.com>
2025-01-26 19:41:19 +0100

    modify RecipeCard

--- mealmate/src/components/RecipeCard.js
+++ mealmate/src/components/RecipeCard.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 
-function RecipeCard({ recipe }) {
-    return (
-        <div className="recipe-card">
-            <img src={recipe.image} alt={recipe.title} />
-            <h3>{recipe.title}</h3>
-            <a href={`https://spoonacular.com/recipes/${recipe.id}`} target="_blank" rel="noopener noreferrer">
-                View Recipe
-            </a>
-        </div>
-    );
+function RecipeCard({ recipe, addToWishlist }) {
+  return (
+    <div className="recipe-card">
+      <img src={recipe.image} alt={recipe.title} />
+      <h3>{recipe.title}</h3>
+      <button className="wishlist-btn" onClick={() => addToWishlist(recipe)}>
+        <FaHeart />
+      </button>
+      <a
+        href={`https://spoonacular.com/recipes/${recipe.id}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View Recipe
+      </a>
+    </div>
+  );
 }
 
 export default RecipeCard;
