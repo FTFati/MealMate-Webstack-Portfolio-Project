242767a3ca3fc5fa83863f4740c38814f95f8124
FTFati <fatima.amkachou@gmail.com>
2025-01-26 19:48:48 +0100

    Update whishlist

--- mealmate/src/components/Wishlist.js
+++ mealmate/src/components/Wishlist.js
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Wishlist() {
-    const [wishlist, setWishlist] = useState([]);
-
-    return (
-        <div>
-            <h2>My Wishlist</h2>
-            {wishlist.length === 0 ? (
-                <p>Your wishlist is empty.</p>
-            ) : (
-                wishlist.map((item, index) => (
-                    <div key={index}>
-                        <p>{item.title}</p>
-                        <button onClick={() => setWishlist(wishlist.filter((_, i) => i !== index))}>
-                            Remove
-                        </button>
-                    </div>
-                ))
-            )}
-        </div>
-    );
+function Wishlist({ wishlist, removeFromWishlist }) {
+  return (
+    <div>
+      <h2>My Wishlist</h2>
+      {wishlist.length === 0 ? (
+        <p>Your wishlist is empty.</p>
+      ) : (
+        wishlist.map((item, index) => (
+          <div key={index}>
+            <p>{item.title}</p>
+            <button onClick={() => removeFromWishlist(index)}>Remove</button>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Wishlist;
